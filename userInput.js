0b5eaecd17524fb9b7d6f724075b3e7ebc99423e
vandries7 <vladandries7@yahoo.com>
2023-04-04 23:58:40 +0300

    add github gist rest api

--- userInput.js
+++ userInput.js
@@ -1,67 +1,91 @@
-function UserInput() {
-    console.log('UserInput module loaded!');
+import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
 
-    let username = '';
+function UserInput() {
+    console.log("UserInput module loaded!");
+
+    let username = "";
     let user = {};
     let gists = [];
 
-    const handleSubmit = (event) => {
+    const octokit = new Octokit({
+        auth: "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",
+    });
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        jQuery.get(`https://api.github.com/users/${username}`, function(response) {
-            user = response;
-            jQuery.get(`https://api.github.com/users/${username}/gists`, function(gistsResponse) {
-                gists = gistsResponse;
-                render();
-            });
+        username = event.target[0].value;
+        user = await octokit.request("GET /users/{username}", {
+            username,
         });
+        gists = await octokit.request("GET /users/{username}/gists", {
+            username,
+        });
+        render();
     };
 
     const render = () => {
-        jQuery('body').append(`
+        jQuery("body").append(`
       <form>
         <input type="text" placeholder="Enter Github username" value="${username}" />
         <button type="submit">Submit</button>
       </form>
     `);
-        jQuery('form').submit(handleSubmit);
-        if (user) {
-            jQuery('body').append(`
+        jQuery("form").submit(handleSubmit);
+        if (user.data) {
+            jQuery("body").append(`
         <div>
-          <img alt="" src="${user.avatar_url}" />
-          <h2>${user.name}</h2>
-          <p>${user.bio}</p>
+          <img alt="" src="${user.data.avatar_url}" />
+          <h2>${user.data.name}</h2>
+          <p>${user.data.bio}</p>
         </div>
       `);
         }
-        if (gists.length > 0) {
-            jQuery('body').append(`
+        if (gists.data.length > 0) {
+            jQuery("body").append(`
         <ul>
-          ${gists.map(gist => `
+          ${gists.data
+                .map(
+                    (gist) => `
             <li>
               <h3>${gist.description}</h3>
               <p>${new Date(gist.created_at).toLocaleDateString()}</p>
-              <span class="badge">${Object.keys(gist.files)[0].split('.').pop()}</span>
+              <span class="badge">${
+                        Object.keys(gist.files)[0].split(".").pop()
+                    }</span>
               <ul>
-                ${gist.forks.map(fork => `
+                ${gist.forks.map((fork) => `
                   <li>${fork.owner.login}</li>
                 `).join('')}
               </ul>
             </li>
-          `).join('')}
+          `
+                )
+                .join("")}
         </ul>
       `);
-            jQuery('li').click(function() {
-                const gist = gists[$(this).index()];
-                jQuery('body').append(`
-          <pre><code class="language-${Object.keys(gist.files)[0].split('.').pop()}">${gist.files[Object.keys(gist.files)[0]].content}</code></pre>
+            jQuery("li").click(function () {
+                const gist = gists.data[$(this).index()];
+                octokit
+                    .request("GET /gists/{gist_id}", {
+                        gist_id: gist.id,
+                        headers: {
+                            "X-GitHub-Api-Version": "2022-11-28",
+                        },
+                    })
+                    .then((response) => {
+                        jQuery("body").append(`
+          <pre><code class="language-${
+                            Object.keys(response.data.files)[0].split(".").pop()
+                        }">${response.data.files[Object.keys(response.data.files)[0]].content}</code></pre>
         `);
-                Prism.highlightAll();
+                        Prism.highlightAll();
+                    });
             });
         }
     };
 
     return {
-        render: render
+        render: render,
     };
 }
 
